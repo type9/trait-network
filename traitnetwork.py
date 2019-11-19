28107ce71ecd4c8ac7998f64b65e4fdeb8bcddfa
type9 <gabespersonal@gmail.com>
2019-11-19 14:01:07 -0800

    Added test data and refactored object format

--- traitnetwork.py
+++ traitnetwork.py
@@ -1,5 +1,4 @@
 from dependencies.dictogram import Dictogram
-from dependencies.markovchain import MarkovChain
 
 class Trait(Dictogram):
     def __init__(self, name):
@@ -12,10 +11,13 @@ class Trait(Dictogram):
 
 class TraitNetwork():
     '''This class accepts a list of object dictionaries in the following format:
-        {'obj_name':
-            'trait1': 0.5,
-            'trait2': 0.25,
-            'trait3': 0.25} 
+        {'name': '<obj_name>' 
+            'traits': {
+                '<unique_trait_name>': 0.5,
+                '<unique_trait_name>': 0.25,
+                '<unique_trait_name>': 0.25
+            }
+        } 
         trait values should represent the weighting of how much it composes of each object adding up to 1.0'''
     def __init__(self, objects):
         # generation variables
@@ -26,6 +28,7 @@ class TraitNetwork():
         self.min_association = 0.3 # minimum value to traverse a node path. if association is lower we ignore the path
 
         # initialization
+        self.generate_nodes(objects)
 
     def create_node(self, name):
         return Trait(name)
@@ -33,7 +36,7 @@ class TraitNetwork():
     def calc_association(self, object, trait_1, trait_2):
         '''This method will return an association value based off of a predetermined formula'''
         association = float()
-        association = object[trait_1] + object[trait_2]
+        association = object['traits'][trait_1] + object['traits'][trait_2]
         return association
 
     def subtract_sets(self, list_1, list_2):
@@ -42,19 +45,21 @@ class TraitNetwork():
         return list(set(list_1) - set(list_2))
     
     def build_associations(self, object):
-            object_traits = object.keys() #
-            traits_checked = list() # keeps track of traits we've already checked in this object
-            for trait in object_traits: # for each trait in that object
-                
-                if trait not in self.nodes.keys(): # if we don't have that trait as a node
-                        self.nodes[trait] = self.create_node(trait) # we create it
+        object_traits = object['traits']
+        # ingredients = object['ingredients']
+        traits_checked = list() # keeps track of traits we've already checked in this object
+        for trait in object_traits: # for each trait in that object
+            
+            if trait not in self.nodes.keys(): # if we don't have that trait as a node
+                    self.nodes[trait] = self.create_node(trait) # we create it
 
-                traits_checked.append(trait) # keeps track of traits we've already checked in this object
-                for other_trait in object_traits.subtract_sets(object_traits, traits_checked): # we look at all other traits that haven't been checked yet
-                    self.nodes[trait][other_trait] += self.calc_association(object, trait, other_trait) # add the association value to the node
+            traits_checked.append(trait) # keeps track of traits we've already checked in this object
+            for other_trait in self.subtract_sets(object_traits, traits_checked): # we look at all other traits that haven't been checked yet
+                association = self.calc_association(object, trait, other_trait)
+                self.nodes[trait].add_count(other_trait, association) # add the association value to the node
     
     def generate_nodes(self, objects):
-        ''' Runtime is O(N^2.5) (where N is the number of objects) because each 
+        ''' Runtime is O(N^2) (where N is the number of objects) because each 
             trait needs to be check against the other traits in each object'''
         for object in objects: # for each object
             self.build_associations(object)
---
+++ traitnetwork_test.py
@@ -0,0 +1,35 @@
+from traitnetwork import TraitNetwork
+from collections import namedtuple
+
+class GenericObj():
+    def __init__(self, name):
+        self.name = name
+        self.traits = {}
+    
+    def add_trait(self, name, value):
+        self.traits[name] = value
+
+drink1 = {'name': 'After Dinner Cocktail',
+        'traits': {'Apricot Brandy': 0.33, 'Lime': 0.33, 'Triple sec': 0.33}}
+
+drink2 = {'name': 'Amaretto Mist',
+        'traits' : {'Amaretto': 0.6, 'Lime': 0.4}}
+
+drink3 = {'name': 'ArchBishop',
+        'traits': {'Gin': 0.4, 'Lime': 0.2, 'Green Ginger Wine': 0.2, 'Benedictine': 0.2}}
+
+
+Drink = namedtuple('Drink', ['name', 'ingredients'])  # like defining a Drink class
+
+drink_list = [drink1, drink2, drink3]
+
+def main():
+    import sys
+    arguments = sys.argv[1:]
+
+    drink_network = TraitNetwork(drink_list)
+    print(drink_network.nodes)
+
+
+if __name__ == '__main__':
+    main()
